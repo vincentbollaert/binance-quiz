deff247c5b26e3f981e8a07be670723f799f9ee2
Vince <vincentbollaert@gmail.com>
2020-02-04 22:47:26 +0700

    added Durstenfeld shuffle

---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+// Durstenfeld shuffle - https://stackoverflow.com/a/12646864
+export function randomiseArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]
+  }
+}
+
+export const placeholder = ''
