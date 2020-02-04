1e1671cd3809da421ca1bbb49be05e79061a51af
Vince <vincentbollaert@gmail.com>
2020-02-04 22:48:08 +0700

    mapping and sorting data for quiz

--- src/pages/Home/reducer.js
+++ src/pages/Home/reducer.js
@@ -4,6 +4,7 @@ import {
   SHAPE_ASYNC_STATUS_SUCCEEDED,
   SHAPE_ASYNC_STATUS_FAILED,
 } from '../../constants/shapeState'
+import { randomiseArray } from '../../utils'
 
 export const GET_DATA_REQUESTED = 'GET_DATA_REQUESTED'
 export const getDataRequested = () => ({
@@ -24,18 +25,13 @@ export const getDataFailed = (error) => ({
 
 export const init = () => ({
   count: 0,
-  data: [],
+  terms: [],
   asyncStatus: SHAPE_ASYNC_STATUS_INITIAL,
 })
 
+
 export default function reducer(state, action) {
   switch (action.type) {
-    case 'increment':
-      return {count: state.count + 1}
-
-    case 'decrement':
-      return {count: state.count - 1}
-
     case GET_DATA_REQUESTED: {
       return {
         ...state,
@@ -44,9 +40,28 @@ export default function reducer(state, action) {
     }
 
     case GET_DATA_SUCCEEDED: {
+      const { payload } = action.payload
+      const NUMBER_OF_QUESTIONS = 10
+      const NUMBER_OF_CHOICES = 3
+
+      const termsByRandom = [...payload]
+      randomiseArray(termsByRandom)
+
+      const choices = termsByRandom.map(({ title }) => title)
+      const incorrectChoices = choices.filter((item, index) => index >= NUMBER_OF_QUESTIONS)
+
+      const termsForQuiz = termsByRandom
+        .filter((item, index) => index < NUMBER_OF_QUESTIONS)
+        .map((term, index) => {
+          const incorrectChoicesForTerm = incorrectChoices.slice(index * NUMBER_OF_CHOICES, (index * NUMBER_OF_CHOICES) + NUMBER_OF_CHOICES)
+          return ({
+            ...term,
+            choices: [term.title, ...incorrectChoicesForTerm]
+          })
+        })
       return {
         ...state,
-        data: action.payload.data,
+        terms: termsForQuiz,
         asyncStatus: SHAPE_ASYNC_STATUS_SUCCEEDED,
       }
     }
@@ -54,7 +69,7 @@ export default function reducer(state, action) {
     case GET_DATA_FAILED: {
       return {
         ...state,
-        asyncStatus: SHAPE_ASYNC_STATUS_FAILED(action.payload || 'could not get data'),
+        asyncStatus: SHAPE_ASYNC_STATUS_FAILED(action.payload || 'could not get glossary'),
       }
     }
 
