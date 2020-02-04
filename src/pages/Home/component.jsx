a006e4eb96c2b0b4609e1589362a42d3190c8860
Vince <vincentbollaert@gmail.com>
2020-02-04 17:12:24 +0700

    added axios, get data

--- src/pages/Home/component.jsx
+++ src/pages/Home/component.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import axios from 'axios'
 
-const Home = () => (
-  <div>
-    This is home
-  </div>
-)
+const Home = () => {
+  function fetchData() {
+    axios.get('https://api.binance.vision/api/glossaries').then(data => {
+      console.log(data)
+    })
+  }
+
+  return (
+    <div>
+      {fetchData()}
+      This is home
+    </div>
+  )
+}
 
 export default Home
