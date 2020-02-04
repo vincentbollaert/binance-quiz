d89981ceb89b7caf38c72bd27d3d37bf73bb4366
Vince <vincentbollaert@gmail.com>
2020-02-04 19:22:14 +0700

    added reusable style variables

---
+++ src/styles/variables.js
@@ -0,0 +1,14 @@
+export const JET = '#333'
+
+// units
+export const UNIT_XSM_INT = 0.4
+export const UNIT_SM_INT = 0.8
+export const UNIT_MD_INT = 1.2
+export const UNIT_LG_INT = 1.6
+export const UNIT_XLG_INT = 2.4
+
+export const UNIT_XSM = `${UNIT_XSM_INT}rem`
+export const UNIT_SM = `${UNIT_SM_INT}rem`
+export const UNIT_MD = `${UNIT_MD_INT}rem`
+export const UNIT_LG = `${UNIT_LG_INT}rem`
+export const UNIT_XLG = `${UNIT_XLG_INT}rem`
