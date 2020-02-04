8a28867ee3abf1503f5f01d7338054fc67c6ccbd
Vince <vincentbollaert@gmail.com>
2020-02-04 19:35:33 +0700

    added spinner to page

--- src/pages/Home/component.jsx
+++ src/pages/Home/component.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useEffect, useReducer } from 'react'
 import axios from 'axios'
 
+import reducer, { init, getDataRequested, getDataSucceeded, getDataFailed } from './reducer'
+import SpinnerLoader from '../../components/Spinner/component'
+
+
 const Home = () => {
-  function fetchData() {
-    axios.get('https://api.binance.vision/api/glossaries').then(data => {
-      console.log(data)
-    })
-  }
+  const [state, dispatch] = useReducer(reducer, undefined, init)
+  const { asyncStatus } = state
+
+  useEffect(() => {
+    dispatch(getDataRequested())
+    axios.get('http://api.binance.vision/api/glossaries/')
+      .then(data => dispatch(getDataSucceeded({ payload: data })))
+      .catch(error => dispatch(getDataFailed({ payload: error })))
+  }, [])
 
   return (
     <div>
-      {fetchData()}
       This is home
+      <SpinnerLoader asyncStatus={asyncStatus} />
     </div>
   )
 }
