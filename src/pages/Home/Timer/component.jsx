5188969ce580764dff95f0247e0e51303841321f
Vince <vincentbollaert@gmail.com>
2020-02-05 15:10:41 +0700

    added timer component

---
+++ src/pages/Home/Timer/component.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+
+const Timer = ({ timeRemaining, setTimer }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => setTimer(timeRemaining - 1), 1000)
+    if (timeRemaining === 1) {
+      clearTimeout(timeout)
+    }
+  })
+
+  return (
+    <div>
+      {timeRemaining}
+    </div>
+  )
+}
+
+export default Timer
