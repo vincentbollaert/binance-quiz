2543bb3bd4396d12d645a81f8fa3ded8e926ae9d
Vince <vincentbollaert@gmail.com>
2020-02-05 16:09:12 +0700

    added a reset and basic page styling

--- src/Application/component.jsx
+++ src/Application/component.jsx
@@ -1,26 +1,43 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
 
+import { UNIT_LG_INT } from '../styles'
+import reset from '../styles/reset'
 import Home from '../pages/Home/component'
 
 export const CLASSNAME_APPLICATION = 'application'
 const GlobalStyle = createGlobalStyle`
-  html {
-    font-family: 'Open Sans', sans-serif;
-    color: #444;
-  }
+  ${reset};
+`
+
+const STYLE_HEADER_HEIGHT_INT = 6.4
+const STYLE_PAGE_MARGIN = `${STYLE_HEADER_HEIGHT_INT + UNIT_LG_INT}rem} auto 0`
+
+const Header = styled.div`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: ${STYLE_HEADER_HEIGHT_INT}rem;
+  background-color: #202020;
+`
+const PageWrap = styled.div`
+  margin: ${STYLE_PAGE_MARGIN};
+  padding: 32px;
+  max-width: 1140px;
+  background-color: #353535;
 `
 
 const Application = () => {
   return (
     <>
       <GlobalStyle />
-      <div>
+      <Header />
+      <PageWrap>
         <Switch>
           <Route path="/" component={Home} />
         </Switch>
-      </div>
+      </PageWrap>
     </>
   )
 }
