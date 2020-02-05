eb0ba338c66e8ccd3d0b8fdd50fbefd59be8e9cf
Vince <vincentbollaert@gmail.com>
2020-02-05 19:47:01 +0700

    styyling

--- src/pages/Home/component.jsx
+++ src/pages/Home/component.jsx
@@ -14,35 +14,56 @@ import reducer, {
 import SpinnerLoader from '../../components/Spinner/component'
 import Timer from './Timer/component'
 
-const Questions = styled.div`
-  max-width: 600px;
+const QuizWrap = styled.div`
+  max-width: 750px;
   margin: 0 auto;
+  display: flex;
 `
+const Questions = styled.div``
 const Question = styled.div`
   display: ${props => props.isQuizComplete || props.isActiveQuestion ? 'block' : 'none'};
-  padding: 24px;
-  margin-bottom: 12px;
+  padding: 46px;
   background-color: #ffffff1c;
+  padding-left: 0;
+  padding-right: 0;
 `
 const Description = styled.div`
+  font-size: 18px;
+  margin-bottom: 70px;
+  padding: 0 46px;
 `
 
 const Answers = styled.div`
-  display: flex;
-  justify-content: center;
-  margin-top: 16px;
+  font-size: 15px;
 `
 const Answer = styled.div`
   background: #444;
-  padding: 4px 8px;
+  padding: 16px 24px 16px 49px;
   color: #eee;
-  margin: 0 4px;
+  margin: 10px 0;
+  display: flex;
+  align-items: baseline;
+  position: relative;
 
   ${props => props.isQuizComplete && `
-    ${props.isCorrectAnswer && 'background-color: green'};
-    ${props.isSelectedAnswer && 'background-color: blue'};
-    ${props.isCorrectAnswer && props.isSelectedAnswer && 'background-color: pink'};
+    ${props.isCorrectAnswer && 'box-shadow: inset 0 0 0 2px #5cf2aa; color: #5cf2aa;'};
+    ${props.isSelectedAnswer && 'box-shadow: inset 0 0 0 2px #f25c5c; color: #f25c5c'};
+    ${props.isCorrectAnswer && props.isSelectedAnswer && 'box-shadow: inset 0 0 0 2px #5cf2aa; color: #5cf2aa;'};
   `}
+`
+const Icon = styled.div`
+  position: absolute;
+  left: 0px;
+  height: 49px;
+  width: 49px;
+  line-height: 49px;
+  text-align: center;
+  top: 0;
+  color: #5cf2aa;
+`
+const AdditionalInformation = styled.div`
+  font-size: 12px;
+  margin-left: auto;
 `
 
 const SelectedAnswers = styled.div`
@@ -54,7 +75,14 @@ const SelectedAnswers = styled.div`
 `
 
 const Next = styled.div`
-  background-color: red;
+  background: #444;
+  width: 114px;
+  height: 114px;
+  flex-shrink: 0;
+  text-align: center;
+  line-height: 114px;
+  text-transform: uppercase;
+  font-size: 14px;
 `
 
 const Home = () => {
@@ -68,7 +96,6 @@ const Home = () => {
     totalTime,
     asyncStatus
   } = state
-  // const { id, title, slug, excerpt, answers, isFinalQuestion } = activeQuestion
   const childRef = useRef()
 
   useEffect(() => {
@@ -102,33 +129,37 @@ const Home = () => {
       <SelectedAnswers isShow={isQuizComplete}>
         Total time: {totalTime} seconds
       </SelectedAnswers>
-      <Questions>
-        {questions.map(({ id, title, slug, excerpt, answers, isFinalQuestion }) => (
-          <Question isQuizComplete={isQuizComplete} isActiveQuestion={id === activeQuestion.id}>
-            <Description>{excerpt}</Description>
-            <Answers>
-              {answers.map(answer => (
-                <Answer
-                  key={answer}
-                  isCorrectAnswer={title === answer}
-                  isSelectedAnswer={(selectedAnswers.filter(selectedAnswer => selectedAnswer.id === id)[0] || {}).selectedAnswer === answer}
-                  isQuizComplete={isQuizComplete}
-                  onClick={() => onSelectAnswer({
-                    id,
-                    selectedAnswer: answer,
-                    correctAnswer: title,
-                    timeToChoose: onGetTimeRemaining(),
-                    isFinalQuestion,
-                  })}
-                >
-                  {answer}
-                </Answer>
-              ))}
-            </Answers>
-          </Question>
-        ))}
-      </Questions>
-      {!isQuizComplete && <Next onClick={() => onNextClick({ activeQuestionId: activeQuestion.id })}>Next</Next>}
+      <QuizWrap>
+        <Questions>
+          {questions.map(({ id, title, slug, excerpt, answers, isFinalQuestion }) => (
+            <Question isQuizComplete={isQuizComplete} isActiveQuestion={id === activeQuestion.id}>
+              <Description>{excerpt}</Description>
+              <Answers>
+                {answers.map(answer => (
+                  <Answer
+                    key={answer}
+                    isCorrectAnswer={title === answer}
+                    isSelectedAnswer={(selectedAnswers.filter(selectedAnswer => selectedAnswer.id === id)[0] || {}).selectedAnswer === answer}
+                    isQuizComplete={isQuizComplete}
+                    onClick={() => onSelectAnswer({
+                      id,
+                      selectedAnswer: answer,
+                      correctAnswer: title,
+                      timeToChoose: onGetTimeRemaining(),
+                      isFinalQuestion,
+                    })}
+                  >
+                    <Icon>O</Icon>
+                    {answer}
+                    <AdditionalInformation>30% blah blah</AdditionalInformation>
+                  </Answer>
+                ))}
+              </Answers>
+            </Question>
+          ))}
+        </Questions>
+        {!isQuizComplete && <Next onClick={() => onNextClick({ activeQuestionId: activeQuestion.id })}>Next</Next>}
+      </QuizWrap>
     </div>
   )
 }
