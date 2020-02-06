b26b68329017daaa6a22972e7fb971c9af657e4c
Vince <vincentbollaert@gmail.com>
2020-02-07 00:24:42 +0700

    removed spinner loader and added line loader instead

---
+++ src/components/Loader/Line/component.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { string, shape } from 'prop-types'
+import styled, { keyframes } from 'styled-components'
+import { PROP_ASYNC_STATUS } from '../../../constants'
+import { SELECTIVE_YELLOW } from '../../../styles'
+
+const transition = keyframes`
+  from { left: -20rem; width: 30%; }
+  50%  { width: 30%; }
+  70%  { width: 70%; }
+  80%  { left: 50%; }
+  95%  { left: 120%; }
+  to   { left: 100%; }
+`
+
+const Wrap = styled.div`
+  z-index: 4;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  height: 3px;
+  overflow: hidden;
+
+  &::before {
+    content: "";
+    display: block;
+    position: absolute;
+    left: -20rem;
+    width: 20rem;
+    height: 100%;
+    background-color: ${SELECTIVE_YELLOW};
+    animation: ${transition} 2s linear infinite;
+  };
+`
+
+class LineLoader extends React.PureComponent {
+  render() {
+    const { asyncStatus, className } = this.props
+    const { isBusy } = asyncStatus
+
+    return (
+      isBusy ? <Wrap className={className} /> : null
+    )
+  }
+}
+
+LineLoader.propTypes = {
+  asyncStatus: shape(PROP_ASYNC_STATUS).isRequired,
+  className: string,
+}
+
+LineLoader.defaultProps = {
+  className: '',
+}
+
+export default LineLoader
