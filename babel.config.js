f52aa7ee257098c7175d460dfb0bb8b5fe55a5d0
Vince <vincentbollaert@gmail.com>
2020-02-07 23:47:57 +0700

    config is no longer a function. perhaps that's why jest was not working

--- babel.config.js
+++ babel.config.js
@@ -1,38 +1,33 @@
-module.exports = api => {
-  const isTest = api.env('test')
-
-  return {
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            node: 'current',
-          },
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
         },
-        '@babel/preset-react',
-      ],
-    ],
-    plugins: [
-      'react-hot-loader/babel',
-      'syntax-dynamic-import',
-      '@babel/plugin-proposal-class-properties'
-    ],
-    env: {
-      production: {
-        plugins: [
-          '@babel/plugin-transform-react-inline-elements',
-          'transform-react-remove-prop-types',
-          '@babel/plugin-transform-react-constant-elements' // hoists react elements, preventing unneccesary reinstantiations
-        ]
       },
-      test: {
-        plugins: [
-          '@babel/plugin-transform-modules-commonjs',
-          'babel-plugin-transform-es2015-modules-commonjs', // not working
-          'dynamic-import-node' // try this one too why not
-        ]
-      }
+      '@babel/preset-react',
+    ],
+  ],
+  plugins: [
+    'react-hot-loader/babel',
+    'syntax-dynamic-import',
+    '@babel/plugin-proposal-class-properties'
+  ],
+  env: {
+    production: {
+      plugins: [
+        '@babel/plugin-transform-react-inline-elements',
+        'transform-react-remove-prop-types',
+        '@babel/plugin-transform-react-constant-elements' // hoists react elements, preventing unneccesary reinstantiations
+      ]
+    },
+    test: {
+      plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+        'babel-plugin-transform-es2015-modules-commonjs',
+      ]
     }
   }
 }
