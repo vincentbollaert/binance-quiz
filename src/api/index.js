bdeee88cf5d9614892e8911c08f5c600363c3fcb
Vince <vincentbollaert@gmail.com>
2020-02-07 10:59:52 +0700

    extracted api endpoint

---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+const API_PROXY = 'https://api.allorigins.win/raw?url='
+const API_BINANCE_GLOSSARIES = 'http://api.binance.vision/api/glossaries'
+
+export const API_BINANCE_PROXIED = `${API_PROXY}${API_BINANCE_GLOSSARIES}`
+export const plceholder = ''
