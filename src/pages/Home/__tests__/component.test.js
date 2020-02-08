a0a9b56d69fd4d7d00bfe593d3478a2e930aa604
Vince <vincentbollaert@gmail.com>
2020-02-08 21:30:47 +0700

    minor changes to test. still lots to do

--- src/pages/Home/__tests__/component.test.js
+++ src/pages/Home/__tests__/component.test.js
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom'
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, act } from '@testing-library/react'
 import axiosMock from 'axios'
 import '@testing-library/jest-dom/extend-expect'
 
@@ -9,12 +9,15 @@ import Home from '../component'
 test('loads data', async () => {
   axiosMock.get.mockImplementationOnce(() => (
     Promise.resolve({
-      data: [{}, {}]
+      data: [{ id: 0, answers: ['1', '2'] }, { id: 1, answers: ['3', '4'] }]
     })
   ))
 
-  const { queryByTestId } = await render(<Home />)
-  const loadingNode = await queryByTestId('component-line-loader')
+  let test
+  await act(async () => {
+    test = await render(<Home />)
+  })
+  const loadingNode = await test.queryByTestId('component-line-loader')
 
   expect(axiosMock.get).toHaveBeenCalledTimes(1)
   expect(loadingNode).toBeFalsy()
