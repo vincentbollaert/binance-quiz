058b23d69cdb91b6adc0b887bc43c99aa0ff5e7f
Vince <vincentbollaert@gmail.com>
2020-02-09 18:26:22 +0700

    updated old tests

--- src/components/Radio/component.js
+++ src/components/Radio/component.js
@@ -80,7 +80,7 @@ export const RadioStyled = styled.div`
 `
 
 const Radio = ({ isQuestionFinished, accentColor, id, className }) => (
-  <Wrap className={className}>
+  <Wrap className={className} data-testid="component-radio">
     <RadioWrap accentColor={accentColor} isDisabled={isQuestionFinished}>
       <RadioField
         accentColor={accentColor}
