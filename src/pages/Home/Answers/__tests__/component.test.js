2710ecb616173d030fdbf1549fd75c04dbb6cc52
Vince <vincentbollaert@gmail.com>
2020-04-15 19:02:44 +0700

    update tests to work with context

--- src/pages/Home/Answers/__tests__/component.test.js
+++ src/pages/Home/Answers/__tests__/component.test.js
@@ -1,24 +1,32 @@
 import '@testing-library/jest-dom'
 import React from 'react'
 import { render } from '@testing-library/react'
+import { QuizContext } from '../../../../context/QuizContext'
 import { selectedAnswer, questions, activeQuestion } from '../../../../constants'
 import Answers from '../component'
 
 describe('Answers', () => {
-  const props = {
+  const context = {
     isQuizComplete: false,
     selectedAnswers: [],
-    activeQuestion,
     allQuestions: questions,
+  }
+  const props = {
+    activeQuestion,
     onSelectAnswer() {},
     onGetTimeToChoose() {},
   }
+  const tree = (contextOverride) => (
+    <QuizContext.Provider value={{ ...context, ...contextOverride}}>
+      <Answers {...props} />
+    </QuizContext.Provider>
+  )
   test('matches snapshot', () => {
-    const { container } = render(<Answers {...props} />)
+    const { container } = render(tree())
     expect(container.firstChild).toMatchSnapshot()
   })
   test('renders elements', () => {
-    const { queryByTestId, queryAllByTestId, queryByText } = render(<Answers {...props} />)
+    const { queryByTestId, queryAllByTestId, queryByText } = render(tree())
     expect(queryAllByTestId('component-answer')).toHaveLength(4)
     expect(queryAllByTestId('component-radio')).toHaveLength(4)
     expect(queryAllByTestId('component-additional-info')).toHaveLength(4)
@@ -29,12 +37,12 @@ describe('Answers', () => {
     expect(queryByText(props.activeQuestion.answers[3])).toBeTruthy()
   })
   test('renders tooltips conditionally', () => {
-    const updatedProps = {
-      ...props,
+    const updatedContext = {
+      ...context,
       isQuizComplete: true,
       selectedAnswers: [selectedAnswer],
     }
-    const { queryAllByTestId } = render(<Answers {...updatedProps} />)
+    const { queryAllByTestId } = render(tree(updatedContext))
     expect(queryAllByTestId('component-tooltip')).toHaveLength(1)
   })
 })
