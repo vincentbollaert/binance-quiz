a44d62b9927a15eabf840324f4bc500593f8f367
Vince <vincentbollaert@gmail.com>
2020-04-15 19:31:51 +0700

    updated additional tests

--- src/pages/Home/__tests__/component.test.js
+++ src/pages/Home/__tests__/component.test.js
@@ -3,8 +3,9 @@ import React from 'react'
 import { render, act } from '@testing-library/react'
 import axiosMock from 'axios'
 import '@testing-library/jest-dom/extend-expect'
+import { QuizContext, QuizContextDispatch } from '../../../context/QuizContext'
 
-import { questions } from '../../../constants'
+import { questions, SHAPE_ASYNC_STATUS_INITIAL, activeQuestion } from '../../../constants'
 import Home from '../component'
 
 describe('home', () => {
@@ -16,8 +17,24 @@ describe('home', () => {
     ))
 
     let component
+    function dispatch() {}
+
     await act(async () => {
-      component = await render(<Home />)
+      component = await render(
+        <QuizContext.Provider value={{
+          isQuizComplete: false,
+          questions: [activeQuestion],
+          activeQuestion: activeQuestion,
+          asyncStatus: SHAPE_ASYNC_STATUS_INITIAL,
+          selectedAnswers: [],
+          answers: [],
+          allQuestions: questions,
+        }}>
+          <QuizContextDispatch.Provider value={dispatch}>
+            <Home />
+          </QuizContextDispatch.Provider>
+        </QuizContext.Provider>
+      )
     })
     const loadingNode = await component.queryByTestId('component-line-loader')
 
@@ -28,8 +45,8 @@ describe('home', () => {
     expect(component.queryByTestId('component-results')).toBeFalsy()
     expect(component.queryByTestId('component-timer')).toBeTruthy()
 
-    expect(component.queryAllByTestId('component-question')).toHaveLength(10)
-    expect(component.queryAllByTestId('component-answers')).toHaveLength(10)
-    expect(component.queryAllByTestId('component-answer')).toHaveLength(40)
+    expect(component.queryAllByTestId('component-question')).toHaveLength(1)
+    expect(component.queryAllByTestId('component-answers')).toHaveLength(1)
+    expect(component.queryAllByTestId('component-answer')).toHaveLength(4)
   })
 })
